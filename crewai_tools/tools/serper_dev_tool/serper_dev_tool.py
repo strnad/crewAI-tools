a627e1024e0cf1f372e88740d735b31b44360268
Jakub Strnad <jakub.creative@gmail.com>
2024-07-17 02:54:53 +0200

    reverted serper_dev_tool.py
    the new version is broken, reverting to the old one

--- crewai_tools/tools/serper_dev_tool/serper_dev_tool.py
+++ crewai_tools/tools/serper_dev_tool/serper_dev_tool.py
@@ -2,17 +2,9 @@ import os
 import json
 import requests
 
-from typing import Optional, Type, Any
+from typing import Type, Any
 from pydantic.v1 import BaseModel, Field
 from crewai_tools.tools.base_tool import BaseTool
-
-def _save_results_to_file(content: str) -> None:
-    """Saves the search results to a file."""
-    filename = f"search_results_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.txt"
-    with open(filename, 'w') as file:
-        file.write(content)
-    print(f"Results saved to {filename}")
-
 
 class SerperDevToolSchema(BaseModel):
 	"""Input for SerperDevTool."""
@@ -23,39 +15,25 @@ class SerperDevTool(BaseTool):
 	description: str = "A tool that can be used to search the internet with a search_query."
 	args_schema: Type[BaseModel] = SerperDevToolSchema
 	search_url: str = "https://google.serper.dev/search"
-	country: Optional[str] = None
-	location: Optional[str] = None
-	locale: Optional[str] = None
-	n_results: int = Field(default=10, description="Number of search results to return")
-        save_file: bool = Field(default=False, description="Flag to determine whether to save the results to a file")
+	n_results: int = 10
 
 	def _run(
 		self,
 		**kwargs: Any,
 	) -> Any:
+		search_query = kwargs.get('search_query')
+		if search_query is None:
+			search_query = kwargs.get('query')
 
-		search_query = kwargs.get('search_query') or kwargs.get('query')
-		save_file = kwargs.get('save_file', self.save_file)
-		n_results = kwargs.get('n_results', self.n_results)
-
-		payload = { "q": search_query, "num": n_results }
-		if self.country:
-			payload["gl"] = self.country
-		if self.location:
-			payload["location"] = self.location
-		if self.locale:
-			payload["hl"] = self.locale		
-		
-		payload = json.dumps(payload)
-
+		payload = json.dumps({"q": search_query})
 		headers = {
-			'X-API-KEY': os.environ['SERPER_API_KEY'],
-			'content-type': 'application/json'
+				'X-API-KEY': os.environ['SERPER_API_KEY'],
+				'content-type': 'application/json'
 		}
 		response = requests.request("POST", self.search_url, headers=headers, data=payload)
 		results = response.json()
 		if 'organic' in results:
-			results = results['organic'][:self.n_results]
+			results = results['organic']
 			string = []
 			for result in results:
 				try:
@@ -66,11 +44,9 @@ class SerperDevTool(BaseTool):
 							"---"
 					]))
 				except KeyError:
-					continue
+					next
 
 			content = '\n'.join(string)
-			if save_file:
-                		_save_results_to_file(content)
 			return f"\nSearch results: {content}\n"
 		else:
 			return results
